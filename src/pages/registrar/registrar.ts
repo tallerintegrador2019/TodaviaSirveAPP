6a8b87ff729ff905e7a29ad2b813c30284e158c1
rober <rcfernandez@outlook.com.ar>
2019-06-20 14:23:48 -0300

    agregado post a registro

--- src/pages/registrar/registrar.ts
+++ src/pages/registrar/registrar.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
-/**
- * Generated class for the RegistrarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +10,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegistrarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  username
+  pass
+  nombre
+  apellido
+  email
+  imagen
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegistrarPage');
+
   }
 
-}
+  submitUsuario() {
+
+/* 
+     let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+ */
+
+
+    let data = JSON.stringify({
+      username: this.username,
+      pass: this.pass,
+      nombre: this.nombre,
+      apellido: this.apellido,
+      email: this.email,
+      imagen: this.imagen
+    });
+
+    this.http.post( "http://todaviasirve.azurewebsites.net/Api/Usuario",
+                    data,
+                    { headers: {'Content-Type': 'application/json'} }
+                  )
+      .subscribe(res => { alert("success " + res); },
+        (err) => { alert("failed"); }
+      );
+
+      console.log(data);
+
+
+
+  } // cierre metodo
+
+
+} // cierre clase
