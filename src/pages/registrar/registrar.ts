fc426bb84629eee7e8690e7e6506aa314f821eac
rober <rcfernandez@outlook.com.ar>
2019-06-21 09:38:13 -0300

    cambios en registro

--- src/pages/registrar/registrar.ts
+++ src/pages/registrar/registrar.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { FileTransfer, FileTransferObject, FileUploadOptions } from '@ionic-native/file-transfer/ngx';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Observable } from 'rxjs/Observable';
 
 
 @IonicPage()
@@ -20,65 +21,27 @@ export class RegistrarPage {
   email
   imagen
 
-  imageURI: any;
-  imageTitle: any;
-  isImageSelected:boolean = false;
+  data
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, public camera: Camera, private transfer: FileTransfer, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: HttpClient,
+    public camera: Camera,
+    private transfer: FileTransfer,
+    public loadingCtrl: LoadingController
+  ) {
   }
 
   ionViewDidLoad() {
 
   }
 
-  chooseFile() {
-    let options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-      allowEdit: true,
-      encodingType: this.camera.EncodingType.JPEG,
-      targetWidth: 500,
-      targetHeight: 500,
-      saveToPhotoAlbum: false
-    };
-   
-    this.camera.getPicture(options).then((img) => {
-      this.imageURI = img;
-      this.isImageSelected =true;
-    }).catch((reason) => {
-      console.log(reason);
-    });
-
-  }
-
-  doImageUpload() {
-    let loader = this.loadingCtrl.create({ content: "Subiendo..." });
-    loader.present();
-    
-    let filename = this.imageURI.split('/').pop();
-    const fileTransfer: FileTransferObject = this.transfer.create();
- 
-    let options: FileUploadOptions = {
-      fileKey: "file",
-      fileName: filename,
-      chunkedMode: false,
-      mimeType: "image/jpg",
-      params: { 'title': this.imageTitle }
-    };
- 
-    fileTransfer.upload(this.imageURI, "http://todaviasirve.azurewebsites.net/api/usuario",options)
-      .then((res)=>{
-        },(err)=>{
-          console.log(err);
-      });
- 
-  } 
 
   submitUsuario() {
 
     let headers = new HttpHeaders();
-  /*   headers.append('Content-Type', 'application/json'); */
+    headers.append('Content-Type', 'application/json');
+    headers.append('enctype', 'multipart/form-data'); 
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
 
@@ -91,15 +54,15 @@ export class RegistrarPage {
       imagen: this.imagen
     });
 
-    this.http.post( "http://todaviasirve.azurewebsites.net/Api/Usuario",
-                    data,
-                    { headers: {'Content-Type': 'application/json'} }
-                  )
+    this.http.post("http://localhost:55081/Api/Usuario",
+      data,
+      { headers: { 'Content-Type': 'application/json'  } }
+    )
       .subscribe(res => { alert("success " + res); },
         (err) => { alert("failed"); }
       );
 
-      console.log(data);
+    console.log(data);
 
 
 
