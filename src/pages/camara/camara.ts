53775a12f019d413afa7ecc3aaaa133fd41523c6
LucasRanoni <lucasunlam@gmail.com>
2019-07-10 20:18:25 -0300

    otros intentos no funca

--- src/pages/camara/camara.ts
+++ src/pages/camara/camara.ts
@@ -3,10 +3,13 @@ import { IonicPage, NavController, NavParams, Config } from 'ionic-angular';
 import { ToastController } from 'ionic-angular/components/toast/toast-controller';
 import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {storage,initializeApp} from 'firebase';
+import {storage,initializeApp } from 'firebase';
  import { LoadingController } from 'ionic-angular';
 import { errorHandler } from '@angular/platform-browser/src/browser';
 import { FIREBASE_CONFIG } from "../../app/firebase.config";
+// import { AngularFireStorage } from "angularfire2/storage";
+// import { Observable } from "rxjs";
+// import { finalize } from 'rxjs/operators';
 
 @IonicPage()
 @Component({
@@ -15,6 +18,12 @@ import { FIREBASE_CONFIG } from "../../app/firebase.config";
 })
 export class CamaraPage {
 
+   // propiedades de storage
+  //  uploadPercent: Observable<number>;
+  //  downloadURL: Observable<string>;
+  //  imageStorage: string = null;
+
+  responseFirebase;
   elemento;
   image: string = null;
   imagenblob;
@@ -31,7 +40,8 @@ export class CamaraPage {
     private camera: Camera,
     private http: HttpClient,
     public loadingCtrl: LoadingController,
-    public toast : ToastController
+    public toast : ToastController,
+    //private storage1: AngularFireStorage
   ) {
     initializeApp(FIREBASE_CONFIG);
   }
@@ -45,22 +55,48 @@ export class CamaraPage {
       async getPictureCam() {
     
         const options: CameraOptions = {
-          quality: 60,
-          targetHeight: 49,
-          targetWidth: 49,
+          quality: 50,
+          targetHeight: 600,
+          targetWidth: 600,
           destinationType: this.camera.DestinationType.DATA_URL,
-          encodingType: this.camera.EncodingType.PNG,
-          mediaType: this.camera.MediaType.PICTURE
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE,
+          //saveToPhotoAlbum: true
         } 
     
   
-          // const resultado = await this.camera.getPicture(options); 
-          // const imagen = `data:image/jpeg;base64,${resultado}`;
-        
+          const resultado = await this.camera.getPicture(options); 
+          this.image  =  'data:image/jpeg;base64,' + resultado;
+          const imagen = `data:image/jpeg;base64,${resultado}`;
+          const pictures = storage().ref('pictures/miFoto');
+          pictures.putString(resultado, 'DATA_URL');
 
-        try{
+                    
 
-         this.camera.getPicture(options).then((imageData) => {
+
+          //               // otra forma
+          // const task = this.storage1.upload("format/", this.image);
+          // const ref = this.storage1.ref("format/");
+
+          // // observar porcentaje cambiados
+          // this.uploadPercent = task.percentageChanges();
+          // //console.log('Imagen subida!');
+
+          // task.snapshotChanges().pipe(
+          //   finalize(() => {
+          //     this.downloadURL = ref.getDownloadURL()
+          //     //this.mostrarToast(this.downloadURL,7000);
+          //     this.downloadURL.subscribe(url => (
+          //       this.imageStorage = url
+          //       //mostrarToast(this.image,7000);
+          //       //this.mostrarToast(this.downloadURL,7000);
+          //       ));
+          //   })
+          // ).subscribe();
+
+        // try{
+
+        //  this.camera.getPicture(options).then((imageData) => {
               //    // imageData is either a base64 encoded string or a file URI
               //    // If it's base64 (DATA_URL):
               //    //this.uploadPhoto(imageData);
@@ -73,33 +109,36 @@ export class CamaraPage {
               //    //this.mostrarToast(this.myPhoto.name, 20000);
               //let decode = this.b64DecodeUnicode(imageData);
               //this.myPhoto = this.convertDataURIToBinary(imageData);
-              this.image = 'data:image/jpeg;base64,' + imageData;
-              let file1 = fetch(this.image).then(r => r.blob());
+              //this.image = 'data:image/jpeg;base64,' + imageData;
+              //let file1 = fetch(this.image).then(r => r.blob());
               //.then(fil => new File([fil],"imagen.jpg"));
-                  this.elemento = file1;
-              this.submitFoto();
+              //this.elemento = file1;
+              //this.submitFoto();
               //this.subirAPICamara();
               
-              //this.image = 'data:image/JPEG;base64,' + imageData;
+            //this.image = 'data:image/JPEG;base64,' + imageData;
                         
-              // const pictures = storage().ref('pictures/reconocimiento');
-              // pictures.putString(this.image, 'DATA_URL');
+            
+            //  .then(savedPicture => {
+            //    this.responseFirebase = savedPicture.ref.getDownloadURL.name;
+            //    this.mostrarToast(this.responseFirebase,7000);
+            //  });
 
               // this.uploadPhoto(imageData);  
 
             
               
-                }, (err) => {
-                  this.mostrarToast("Fallo en Get Picture", 5000);
-                });
+            //    }, (err) => {
+            //        this.mostrarToast("Fallo en Get Picture"+err.message, 5000);
+            //  });
               //   //const imagen = `data:image/jpeg;base64,${resultado}`;
               //   const pictures = storage().ref('pictures/reconocimiento');
               //   //this.image = resultado;
               //   pictures.putString(this.image, 'DATA_URL');
         
-         } catch (error) {
-           this.mostrarToast("Fallo fuera del Get", 5000);
-        }
+        //  } catch (error) {
+        //    this.mostrarToast("Fallo fuera del Get"+error.message, 5000);
+        // }
      } 
 
      b64DecodeUnicode(str) {
@@ -265,19 +304,20 @@ export class CamaraPage {
   // DESDE ARCHIVO ----------------------
   getPicture(event) {
     this.image = event.target.files[0];
-    this.elemento = this.image;
-    this.submitFoto();
+    const pictures = storage().ref('pictures/miFoto');
+    pictures.putString(this.image, 'DATA_URL');
+    //this.elemento = this.image;
+    //this.submitFoto();
     // this.myPhoto = event.target.files[0];
     // //this.image = 'data:image/JPEG;base64,' + imageData;
     //  this.mostrarToast(this.image, 15000);
     //  console.log(this.image.toString);
-    // let reader = new FileReader();
-    // reader.onload = (event: any) => {
-    //   this.image = event.target.result;
-    // }
-    //   reader.readAsDataURL(event.target.files[0]);
-
-    // this.subirAAPI();
+     let reader = new FileReader();
+     reader.onload = (event: any) => {
+       this.image = event.target.result;
+     }
+       reader.readAsDataURL(event.target.files[0]);
+     this.subirAAPI();
   }  
 
   subirAAPI() {
